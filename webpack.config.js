149c0bcdd8a24bc8c87cebaa0d0665a81eb31f39
leeric92 <hle2806@uw.edu>
2015-09-02 13:57:48 -0700

    add webpack structure of the app

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var ROOT_PATH = path.resolve(__dirname);
+module.exports = {
+  entry: path.resolve(ROOT_PATH, 'app/main'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: { 
+    colors: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  }, 
+plugins: [
+new webpack.HotModuleReplacementPlugin(),
+new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+] };
